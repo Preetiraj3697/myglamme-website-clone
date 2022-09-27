01ac7041ae95a45ae20392fd1f874c76b0b4dc9b
Preetiraj3697 <satendra11kum@gmail.com>
2022-09-27 19:52:12 +0530

    add starting Website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Myglamm Website Clone</h1>
+       <img
+        src="https://files.myglamm.com/site-images/original/700X488_2.gif"
+        alt="this slowpoke moves"
+        width="250"
+      />
+    </div>
+  );
+}
+
+export default App;
