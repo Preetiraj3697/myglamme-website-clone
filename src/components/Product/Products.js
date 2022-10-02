d1377ea35edff2b58c57dd27c7dd18a04c38ef34
Preetiraj3697 <satendra11kum@gmail.com>
2022-10-02 20:21:41 +0530

    day-4

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import LoadingScreen from "react-loading-screen";
+import { useCart } from "react-use-cart";
 import spinner from "./S.gif";
 import { Link} from "react-router-dom";
 import './Products.css'
@@ -8,7 +9,7 @@ export default function Products() {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
   const [isLoding, setIsLoding] = useState(true)
-  
+  const { addItem } = useCart();
   const getApiData = async () => {
     setIsLoding(true);
     try {
@@ -57,27 +58,27 @@ export default function Products() {
         })
         .map((item) => {
           return (
-            <div className="grid-container">
-             <div className="grid-item">
+            <div className="grid-container" >
+             <div className="grid-item" key={item.id}>
               <img src={item.api_featured_image} alt="iamge"/>
               <h4>{item.name}</h4>
               <h2>₹ {item.price}</h2>
-             <button className="btnt">Add to Bag</button>
+             <button className="btnt" onClick={() => addItem(item)}>Add to Bag</button>
               <Link to={`/products/${item.id}`}><button className="btnt" >More Details</button></Link>
               </div>
-              <div className="grid-item">
+              <div className="grid-item" key={item.id}>
               <img src={item.api_featured_image} alt="iamge"/>
               <h4>{item.name}</h4>
               <h2>₹ {item.price}</h2>
-              <button className="btnt">Add to Bag</button>
+              <button className="btnt" onClick={() => addItem(item)}>Add to Bag</button>
               <Link to={`/products/${item.id}`}><button className="btnt" >More Details</button></Link>
               
               </div>
-              <div className="grid-item">
+              <div className="grid-item" key={item.id}>
               <img src={item.api_featured_image} alt="iamge"/>
               <h4>{item.name} </h4>
               <h2>₹ {item.price}</h2>
-              <button className="btnt">Add to Bag</button>
+              <button className="btnt" onClick={() => addItem(item)}>Add to Bag</button>
               <Link to={`/products/${item.id}`}><button className="btnt" >More Details</button></Link>
               </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { CartProvider } from "react-use-cart";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+  <CartProvider>
     <App />
-  </React.StrictMode>
+    </CartProvider>
 );
 
 
